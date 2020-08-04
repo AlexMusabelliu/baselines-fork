0fee8b0a74561ec1c7cb49e76791a45dbe246939
AS <a.musabell@gmail.com>
2020-08-03 23:41:53 -0400

    change run

--- baselines/ppo2/runner.py
+++ baselines/ppo2/runner.py
@@ -46,7 +46,7 @@ def occlude(data, percent=.5, height=84, width=84, gen=None, attention=None):
         print(type(msize))
         ma = tf.slice(m, [int(msize * percent)], [1])
 
-        result = tf.map_fn(lambda x: 0 if x < ma else x, attention)
+        result = tf.map_fn(lambda x: 0 if all(tf.math.less(x, ma)) else x, attention)
         print(f"---*****Size/shape of mod tensor: {tf.size(result)} / {tf.shape(result)}")
         
     return result
